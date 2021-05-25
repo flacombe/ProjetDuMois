3dca561bccc2731b275574b5dc7cb00d9fe6dd53
FLacombe <francois.lacombe@infos-reseaux.com>
2021-05-26 01:10:54 +0200

    Fix extracts

--- projects/2021-01_poteaux/extract.sh
+++ projects/2021-01_poteaux/extract.sh
@@ -2,4 +2,4 @@
 
 # Production du csv
 rm -f /data/files/pdm/extracts/poteaux.csv
-psql -d postgres://localhost:5433/osm -c "COPY (select osm_id, ST_X(St_Transform(geom, 4326)) AS lng, St_Y(St_Transform(geom, 4326)) AS lat, tags->>'power' AS power, tags->>'operator' AS operator, tags->>'material' AS material, tags->>'location:transition' AS ras, tags->>'line_attachment' AS line_attachment, tags->>'line_management' AS line_management FROM pdm_project_poteaux WHERE St_geometryType(geom)='ST_Point') TO STDOUT DELIMITER ',' CSV HEADER" > /data/files/pdm/extracts/poteaux.csv
+psql -d postgres://localhost:5432/osm -c "COPY (select osm_id, ST_X(St_Transform(geom, 4326)) AS lng, St_Y(St_Transform(geom, 4326)) AS lat, tags->>'power' AS power, tags->>'operator' AS operator, tags->>'material' AS material, tags->>'location:transition' AS ras, tags->>'line_attachment' AS line_attachment, tags->>'line_management' AS line_management FROM pdm_project_poteaux WHERE St_geometryType(geom)='ST_Point') TO STDOUT DELIMITER ',' CSV HEADER" > /data/files/pdm/extracts/poteaux.csv
